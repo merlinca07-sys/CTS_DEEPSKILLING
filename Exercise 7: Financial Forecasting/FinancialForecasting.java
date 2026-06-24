195114fa1c4b1559ccf950a7c3edfe08317ce46e
MERLIN C A <merlinca07@gmail.com>
2026-06-24 22:41:18 +0530

    Add FinancialForecasting class for value forecasting

---
+++ Exercise 7: Financial Forecasting/FinancialForecasting.java
@@ -0,0 +1,21 @@
+class FinancialForecasting {
+    static double forecastValue(double currentValue, double growthRate,int years) {
+        if (years == 0) {
+            return currentValue;
+        }
+        return forecastValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1);
+    }
+    public static void main(String[] args) {
+        double presentValue = 10000;
+        double growthRate = 0.10; 
+        int years = 3;
+        double futureValue = forecastValue(presentValue,growthRate,years);
+        System.out.println("Present Value:" + presentValue);
+        System.out.println("Growth Rate: " + (growthRate * 100) + "%");
+        System.out.println("Years: " + years);
+        System.out.println("Future Value:" + futureValue);
+    }
+}
