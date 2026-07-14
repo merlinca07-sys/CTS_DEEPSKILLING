4e33c756b1b69fba92b3a91b3c0d38f56c638ef5
MERLIN C A <merlinca07@gmail.com>
2026-07-14 21:49:38 +0530

    Implement REST - Get country based on country code

---
+++ week3/spring-learn5/src/main/java/com/cognizant/spring_learn/service/CountryService.java
@@ -0,0 +1,35 @@
+package com.cognizant.spring_learn.service;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.spring_learn.model.Country;
+
+@Service
+public class CountryService {
+
+    @SuppressWarnings("unchecked")
+    public Country getCountry(String code) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countryList =
+                (List<Country>) context.getBean("countryList");
+
+        for (Country country : countryList) {
+
+            if (country.getCode().equalsIgnoreCase(code)) {
+                context.close();
+                return country;
+            }
+
+        }
+
+        context.close();
+        return null;
+    }
+
+}
